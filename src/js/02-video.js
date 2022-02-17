a051f626369f881e7c25127ca20b890d13ba9a22
Anna Miklukha <miklukha.anna@ukr.net>
2022-02-17 12:43:34 +0200

    do task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onSetTimeInLocalStorage, 1000));
+setVideoTime(localStorage.getItem(STORAGE_KEY));
+
+function onSetTimeInLocalStorage(data) {
+  const videoCurrentTime = data.seconds;
+
+  localStorage.setItem(STORAGE_KEY, videoCurrentTime);
+}
+
+function setVideoTime(seconds) {
+  player.setCurrentTime(seconds);
+}
