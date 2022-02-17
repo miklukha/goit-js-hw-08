73fa68a8fab389b67e7aba434876b044313470b3
Anna Miklukha <miklukha.anna@ukr.net>
2022-02-17 11:16:19 +0200

    do task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const galleryItemsMarkup = createGalleryItem(galleryItems);
+galleryRef.innerHTML = galleryItemsMarkup;
+
+let gallery = new SimpleLightbox('.gallery a');
+
+gallery.defaultOptions.captionsData = 'alt';
+gallery.defaultOptions.captionDelay = 250;
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>`,
+    )
+    .join('');
+}
