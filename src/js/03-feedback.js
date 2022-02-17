66414686c1a3e7ce7c59e00da9741c378910912e
Anna Miklukha <miklukha.anna@ukr.net>
2022-02-17 13:47:55 +0200

    do task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+const feedbackFormRef = document.querySelector('.feedback-form');
+
+feedbackFormRef.addEventListener('input', throttle(onFieldsFormInput, 500));
+feedbackFormRef.addEventListener('submit', onSubmitForm);
+
+populateFieldForm();
+
+function onFieldsFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  const formDataJSON = JSON.stringify(formData);
+
+  localStorage.setItem(STORAGE_KEY, formDataJSON);
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateFieldForm() {
+  const savedFormData = localStorage.getItem(STORAGE_KEY);
+  const parsedFormData = JSON.parse(savedFormData);
+
+  if (savedFormData) {
+    feedbackFormRef.email.value = parsedFormData.email;
+    feedbackFormRef.message.value = parsedFormData.message;
+  }
+}
